69e3502891353e0be2b7307944635b075410b87b
Carlos Avila <carlosavila.ce@gmail.com>
2012-09-11 15:55:26 -0700

    Sync and Async file reads

--- An Introduction to Node.js/The File System/read.js
+++ An Introduction to Node.js/The File System/read.js
@@ -1,6 +1,13 @@
 var fs = require('fs');
 
-console.log("Starting...");
+// synch version
+console.log("Starting synch version...");
+var data = fs.readFileSync("sample.txt");
+console.log("Contents: " + data);
+console.log("Carry on executing...");
+
+// asynch version
+console.log("Starting asynch version...");
 fs.readFile("sample.txt", function (error, data) {
 	console.log("Contents: " + data);
 });
