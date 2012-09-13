7cb52a61fc5b6171b2d6c77ad8b5ba0b4a0d7153
Carlos Avila <carlosavila.ce@gmail.com>
2012-09-12 17:41:01 -0700

    Complete lesson4 first web server with config read, server, listener and config watch

--- An Introduction to Node.js/MyFirstWebServer/js/server.js
+++ An Introduction to Node.js/MyFirstWebServer/js/server.js
@@ -1,15 +1,39 @@
 var http = require('http');
-console.log("Starting");
+var fs = require('fs');
 
-var host = "127.0.0.1";
-var port = 1337;
+// load config synch
+console.log("Starting");
+var config = JSON.parse(fs.readFileSync("../config.json"));
+var host = config.host;
+var port = config.port;
+
+// create web server
 var server = http.createServer(function (request, response) {
 	console.log("Received request: " + request.url);
-	response.writeHead(200, {"Content-type":"text/plain"});
-	response.write("Hello world!", "utf8");
-	response.end();
+	fs.readFile("./../public" + request.url, function(error, data){
+		if (error) {
+			response.writeHead(404, {"Content-type":"text/plain"});
+			response.end("Oops! can't find the page");
+		} else {
+			response.writeHead(200, {"Content-type":"text/html"});
+			response.end(data);
+		}
+	});
 });
 
+// listen
 server.listen(port, host, function() {
 	console.log("Listening on " + host + ":" + port);
+});
+
+
+// watch config for changes to web server
+fs.watch("../config.json", function(){
+	config = JSON.parse(fs.readFileSync("../config.json"));
+	host = config.host;
+	port = config.port;
+	server.close();
+	server.listen(port, host, function() {
+		console.log("Now listening on " + host + ":" + port);
+	});
 });
